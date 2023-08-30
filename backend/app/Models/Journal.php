75ee93890accb08b8d54b523b1c376c00a6fd515
Alexey Fishler <amberdance@yandex.ru>
2023-08-30 21:35:38 +0300

    fix(api): Убрал кастование в datetime для удобочитаемой даты и времени

--- backend/app/Models/Journal.php
+++ backend/app/Models/Journal.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use DateTime;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -10,36 +10,37 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 /**
- * @property int      $id
- * @property int      $attacker_id
- * @property int      $victim_id
- * @property Datetime $detection_date
- * @property Datetime $group_notice_date
- * @property Datetime $zav_sector_notice_date
- * @property boolean  $is_closed
+ * @property int     $id
+ * @property int     $attacker_id
+ * @property int     $victim_id
+ * @property Carbon  $detection_date
+ * @property Carbon  $group_notice_date
+ * @property Carbon  $zav_sector_notice_date
+ * @property boolean $is_closed
  */
-class Journal extends Model {
+class   Journal extends Model
+{
     use HasFactory, SoftDeletes;
 
     protected $hidden = [
-            "deleted_at",
+        "deleted_at",
     ];
 
     protected $casts = [
-            'detection_date'         => 'datetime',
-            'group_notice_date'      => 'datetime',
-            'zav_sector_notice_date' => 'datetime',
-            "is_closed"              => "boolean",
+        "is_closed" => "boolean",
     ];
 
     protected $fillable = [];
     protected $with = ['attacker', 'victim'];
 
-    public function attacker(): HasOne {
+    public function attacker(): HasOne
+    {
         return $this->hasOne(Attacker::class, "id", "attacker_id");
     }
 
-    public function victim(): HasOne {
+    public function victim(): HasOne
+    {
         return $this->hasOne(Victim::class, "id", "victim_id");
     }
+
 }
