051f0b9121592e8c96806bcb5bed1a33fa024da3
Alexey Fishler <amberdance@yandex.ru>
2023-08-31 00:33:03 +0300

    feature(api): Добавил обработчик ошибок

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Utils\ResponseEntity;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\App;
+use Illuminate\Validation\UnauthorizedException;
+use PDOException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+
+    use ResponseEntity;
+
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -18,13 +27,41 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+
     /**
-     * Register the exception handling callbacks for the application.
+     * @param             $request
+     * @param  Throwable  $e
+     *
+     * @return mixed
+     * @throws Throwable
      */
-    public function register(): void
+    public function render($request, Throwable $e): mixed
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        if ($e instanceof NotFoundHttpException || $e instanceof ModelNotFoundException) {
+            return $this->jsonNotFound();
+        }
+
+
+        if ($e instanceof UnauthorizedException) {
+            return $this->jsonForbidden($e->getMessage());
+        }
+
+        if ($e instanceof PDOException) {
+            $message = $e->getMessage();
+
+            if (is_numeric(stripos($message, "Duplicate"))) {
+                return $this->jsonError(1062,
+                    App::environment("local") ? $e->getMessage() : preg_replace("/1062(?s)(.*) \(SQL/", "",
+                        $e->getMessage()));
+            }
+
+            if (is_numeric(stripos($message, "Integrity constraint violation"))) {
+                return $this->jsonError(1451,
+                    App::environment("local") ? $e->getMessage() : preg_replace("/1451(?s)(.*) \(SQL/", "",
+                        $e->getMessage()));
+            }
+        }
+
+        return parent::render($request, $e);
     }
 }
