0b27593f1b5135ff0ed01ac6dcdcaee94e631a37
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-08-31 15:55:06 +0300

    refactor: Убрал лишнее, добавил страницу авторизации

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="login" id="login-page">
+    <div class="login__title">Авторизация</div>
+    <div class="login__form"></div>
+  </section>
+</template>
+
+<script setup lang="ts">
+console.log("login comp");
+</script>
+
+<style scoped></style>
