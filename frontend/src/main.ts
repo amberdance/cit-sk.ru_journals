2d500b51c8f77f9bf906e5b442ee71c6fcb17a2b
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-09-01 13:45:58 +0300

    feat: Подключение стилей

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,4 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "@/style/style.css";
+import "@/style/normalize.css";
+import "@/style/fonts.css";
+
 createApp(App).use(store).use(router).mount("#app");
