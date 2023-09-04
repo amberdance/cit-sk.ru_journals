45ec7745c313b3d020cc71d1ac9590a5b86ce204
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-09-04 20:47:18 +0300

    fix:  Убрал случайно добавленные ненужные файлы

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,20 +3,17 @@
     <div class="login__title">Авторизация</div>
     <div class="login__form form">
       <app-input />
-
-      <app-btn>Вход</app-btn>
     </div>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import AppBtn from "@/components/uxui/AppBtn.vue";
 import AppInput from "@/components/uxui/AppInput.vue";
 
 export default defineComponent({
   name: "LoginView",
-  components: { AppInput, AppBtn },
+  components: { AppInput },
 });
 </script>
 
