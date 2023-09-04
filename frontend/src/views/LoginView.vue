160e0ea1133e195eddaf578a5c78cec3d8b63fdd
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-09-04 13:11:50 +0300

    feat: Набросал скелет верстки страницы логина

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,12 +1,33 @@
 <template>
   <section class="login" id="login-page">
     <div class="login__title">Авторизация</div>
-    <div class="login__form"></div>
+    <div class="login__form form">
+      <div class="form__login">
+        <input name="login" type="text" id="login" />
+        <label for="login">Логин</label>
+      </div>
+
+      <div class="form__password">
+        <input name="password" type="text" id="password" />
+        <label for="password">Пароль</label>
+      </div>
+
+      <app-btn>Вход</app-btn>
+    </div>
   </section>
 </template>
 
-<script setup lang="ts">
-console.log("login comp");
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppBtn from "@/components/uxui/AppBtn.vue";
+
+export default defineComponent({
+  name: "LoginView",
+  components: { AppBtn },
+  setup() {
+    console.log("LoginPage");
+  },
+});
 </script>
 
 <style scoped></style>
