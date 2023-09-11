eddf212cdf966aefdc78a8c47a7e248aa852c5c2
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-09-11 08:49:22 +0300

    feat: Форма авторизации

--- frontend/src/views/AuthorizationView.vue
+++ frontend/src/views/AuthorizationView.vue
@@ -1,15 +1,49 @@
 <template>
   <section id="auth">
+    <el-form class="auth-form">
+      <el-form-item>
+        <el-input placeholder="Логин" v-model="authForm.login" />
+      </el-form-item>
 
+      <el-form-item>
+        <el-input
+          placeholder="Пароль"
+          v-model="authForm.password"
+          type="password"
+          autocomplete="off"
+          clearable
+        />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Войти</el-button>
+      </el-form-item>
+    </el-form>
   </section>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import { reactive } from "vue";
 
-export default defineComponent({
-  name: "AuthorizationView",
+const authForm = reactive({
+  login: "",
+  password: "",
 });
+
+const onSubmit = () => {
+  console.log("authBtn");
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+#auth {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.auth-form {
+  width: 300px;
+}
+</style>
